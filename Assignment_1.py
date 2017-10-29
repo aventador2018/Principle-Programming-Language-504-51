f42ed3569e6fbf28e8687d44a623a1eaf9fddaf1
aventador2018 <zl272@njit.edu>
2017-10-29 13:46:50 -0400

    Exercise 1.5

--- Assignment_1.py
+++ Assignment_1.py
@@ -8,3 +8,16 @@ print i+j
 print "Hello, World!"
 
 #Exercise 1.3. Derive and compute a formula.
+year = 10.0**9/(365*24*60*60)
+print "%.1f years" %(year)
+
+#Exercise 1.4. Convert from meters to British length units.
+meter = 640
+inch = meter*100/2.54
+foot = inch/12
+yard = foot/3
+mile = yard/1760
+
+print "%.2f inches, %.2f feet, %.2f yards, %.4f miles" %(inch, foot, yard, mile)
+
+#Exercise 1.5. Compute the mass of various substances.
