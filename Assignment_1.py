e9579b1afba397d843157255f27e2bf24f75055f
aventador2018 <zl272@njit.edu>
2017-11-09 22:18:02 -0500

    test

--- Assignment_1.py
+++ Assignment_1.py
@@ -21,3 +21,13 @@ mile = yard/1760
 print "%.2f inches, %.2f feet, %.2f yards, %.4f miles" %(inch, foot, yard, mile)
 
 #Exercise 1.5. Compute the mass of various substances.
+print 1000*7.8 #mass of iron
+print 1000*0.0012 #mass of air
+print 1000*0.67 #mass of gas
+print 1000*0.9 #mass of ice
+print 1000*1.03 #mass of the human body
+print 1000*10.5 #mass of the human silver
+print 1000*21.4 #mass of the human platinum
+#iron, air, gasoline, ice, the human,body, silver, and platinum: 21.4.
+
+#Exercise 1.6. Compute the growth of money in a bank.
